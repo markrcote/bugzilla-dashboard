8cd8c5078e3f648c00e10f6c8f2681f636f89cb7
Mark Cote <mcote@mozilla.com>
2010-10-28 15:29:48 -0400

    Moved team list to its own file.

---
+++ js/modules/teams.js
@@ -0,0 +1,20 @@
+Require.modules["teams"] = function(exports) {
+  var default_teams = {
+    foo: {
+      name: "Team Foo",
+      members: [
+        ["Mark Cote", "mcote@mozilla.com"]
+      ]
+    },
+    bar: {
+      name: "Team Bar",
+      members: [
+        ["Clint Talbert", "ctalbert@mozilla.com"]
+      ]
+    }
+  };
+  
+  exports.get = function get() {
+    return default_teams;
+  };
+};
