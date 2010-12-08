26ae1a7159be388683c99cf55ed4b29042a01508
Mark Cote <mcote@mozilla.com>
2010-12-08 18:09:35 -0500

    Don't cache errors from server, if we detect them.

--- server/bugcache.py
+++ server/bugcache.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import urllib
 
 QUERIES_TABLE = 'queries'
@@ -18,8 +19,15 @@ class BugCache(object):
     def load_data(self, search_string):
         url = self.bzapi_url + search_string
         #print 'url is %s' % url
-        json_data = urllib.urlopen(url).read()
-        
+        attempts = 0
+        while attempts < 3:
+            json_data = urllib.urlopen(url).read()
+            tmp_json = json.loads(json_data)
+            if 'error' not in tmp_json or not tmp_json['error']:
+                break
+            attempts += 1
+        if attempts == 3:
+            return ''
         return json_data
     
     def get(self, search_string):
@@ -27,13 +35,15 @@ class BugCache(object):
         cached_rsp = self.check_cache(search_string)
         for r in cached_rsp:
             cached = r
-        if not cached or datetime.datetime.now() > (cached.lastmod + CACHE_EXPIRY):
-            results = self.load_data(search_string)
+        if cached and datetime.datetime.now() < (cached.lastmod + CACHE_EXPIRY):
+            return cached.results
+
+        results = self.load_data(search_string)
+        if results:
             if cached:
                 self.db.update(QUERIES_TABLE, vars={'search': search_string},
-                              where="search = $search", search=search_string, results=results,
-                              lastmod=datetime.datetime.now())
+                               where="search = $search", search=search_string, results=results,
+                               lastmod=datetime.datetime.now())
             else:
                 self.db.insert(QUERIES_TABLE, search=search_string, results=results)
-            return results
-        return cached.results
+        return results
